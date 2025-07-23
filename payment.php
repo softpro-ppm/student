7741c53e771e0a1629552e35c58e2970211c42d4
softpro-ppm <softpro.ppm@gmail.com>
2025-07-23 10:45:34 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -38,7 +38,7 @@ if (strlen($_SESSION['alogin']) == "") {
             if($row['balance'] == ''){
                 $Balance_val = $row['total_fee'];
             }else{
-                //echo $Balance_val = $row['balance'];
+                $Balance_val = $row['balance'];
                 $Balance_val = $row['total_fee'];
             }
             
